12a01ec2d77977063a8e6bec955a6d9903803d89
0xfbad <106453412+0xfbad@users.noreply.github.com>
2026-08-30 22:48:35 -0400

    enforce full verification gates

--- flake.nix
+++ flake.nix
@@ -15,15 +15,38 @@
   in {
     formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
 
-    checks = eachSystem (pkgs: {
+    checks = eachSystem (pkgs: let
+      pythonEnv = pkgs.python3.withPackages (ps: [ps.mypy ps.pytest ps.vulture ps.markupsafe ps.jinja2 ps.pyyaml]);
+    in {
       formatting = treefmtEval.${pkgs.system}.config.build.check self;
+      tests = pkgs.runCommand "ctfd-remote-desktop-tests" {nativeBuildInputs = [pythonEnv pkgs.util-linux];} ''
+        cd ${self}
+        pytest -p no:cacheprovider tests -q
+        touch $out
+      '';
+      types = pkgs.runCommand "ctfd-remote-desktop-types" {nativeBuildInputs = [pythonEnv];} ''
+        cd ${self}
+        mypy --cache-dir "$TMPDIR/mypy" .
+        touch $out
+      '';
+      dead-code = pkgs.runCommand "ctfd-remote-desktop-dead-code" {nativeBuildInputs = [pythonEnv];} ''
+        cd ${self}
+        vulture .
+        touch $out
+      '';
+      shell = pkgs.runCommand "ctfd-remote-desktop-shell" {nativeBuildInputs = [pkgs.shellcheck];} ''
+        cd ${self}
+        shellcheck setup.sh
+        bash -n setup.sh
+        touch $out
+      '';
     });
 
     devShells = eachSystem (pkgs: {
       default = pkgs.mkShell {
         packages = with pkgs; [
           uv
-          (python3.withPackages (ps: [ps.ruff ps.mypy ps.pytest ps.vulture ps.markupsafe ps.jinja2]))
+          (python3.withPackages (ps: [ps.ruff ps.mypy ps.pytest ps.vulture ps.markupsafe ps.jinja2 ps.pyyaml]))
         ];
         shellHook = ''
           echo "ruff check .          lint"
